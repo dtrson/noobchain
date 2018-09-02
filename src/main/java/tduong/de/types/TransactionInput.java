a44a60ad062ce4e7ccee0ab53aaf047acdd74189
Son Duong <dtrson@live.de>
2018-09-02 15:45:54 +0200

    added TransactionInput and TransactionOutput

--- src/main/java/tduong/de/types/TransactionInput.java
+++ src/main/java/tduong/de/types/TransactionInput.java
@@ -4,5 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class TransactionInput {
+	private String transactionOutputId;
+	private TransactionOutput UTXO;
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
 
 }
--- src/main/java/tduong/de/types/TransactionOutput.java
+++ src/main/java/tduong/de/types/TransactionOutput.java
@@ -1,5 +1,27 @@
 package tduong.de.types;
 
-public class TransactionOutput {
+import java.security.PublicKey;
 
+import lombok.Getter;
+import tduong.de.utils.HashUtil;
+
+@Getter
+public class TransactionOutput {
+	private String id;
+	private PublicKey reciepient; // also known as the new owner of these coins.
+	private float value;
+	private String parentTransactionId; // the id of the transaction this output was created in
+
+	public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
+		this.reciepient = reciepient;
+		this.value = value;
+		this.parentTransactionId = parentTransactionId;
+		this.id = HashUtil
+				.applySha256(HashUtil.getStringFromKey(reciepient) + Float.toString(value) + parentTransactionId);
+	}
+
+	// check if coin belongs to you
+	public boolean isMine(PublicKey publicKey) {
+		return publicKey == reciepient;
+	}
 }
